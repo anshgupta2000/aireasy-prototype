724b0838997537161f7e2eaf9d51e746251f4a3d
Ansh Gupta <48497677+anshgupta2000@users.noreply.github.com>
2023-05-17 21:42:02 -0400

    Add files via upload

---
+++ Aireasy/lib/models/user.dart
@@ -0,0 +1,69 @@
+class User {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
+  final String gender;
+  final DateTime birthday;
+  final String hometown;
+  final String aboutMe;
+  final String profession;
+  final String interests;
+  final String role; // "passenger" or "flight attendant"
+  final String? flightReference; // This can be null for flight attendants
+
+  User({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+    required this.gender,
+    required this.birthday,
+    required this.hometown,
+    required this.aboutMe,
+    required this.profession,
+    required this.interests,
+    required this.role,
+    this.flightReference,
+  });
+
+  // Method to convert a User object into a map object
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'gender': gender,
+      'birthday': birthday.toIso8601String(),
+      'hometown': hometown,
+      'aboutMe': aboutMe,
+      'profession': profession,
+      'interests': interests,
+      'role': role,
+      'flightReference': flightReference,
+    };
+  }
+
+  // Method to convert a map object into a User object
+  static User fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'],
+      firstName: map['firstName'],
+      lastName: map['lastName'],
+      email: map['email'],
+      phoneNumber: map['phoneNumber'],
+      gender: map['gender'],
+      birthday: DateTime.parse(map['birthday']),
+      hometown: map['hometown'],
+      aboutMe: map['aboutMe'],
+      profession: map['profession'],
+      interests: map['interests'],
+      role: map['role'],
+      flightReference: map['flightReference'],
+    );
+  }
+}
