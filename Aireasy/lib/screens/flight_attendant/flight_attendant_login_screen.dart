671ee3d8f3337d8c7adba1e80b25dfeaaf9e6350
Ansh Gupta <48497677+anshgupta2000@users.noreply.github.com>
2023-05-25 04:29:06 -0400

    placeholder code for flight_attendant_login_screen.dart

---
+++ Aireasy/lib/screens/flight_attendant/flight_attendant_login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FlightAttendantLoginScreen extends StatelessWidget {
+  const FlightAttendantLoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flight Attendant Login Screen'),
+      ),
+      body: const Center(
+        child: Text('Flight Attendant Login Screen'),
+      ),
+    );
+  }
+}
