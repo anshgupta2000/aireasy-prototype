9178a5bf33facecb109c72ed30d288f8c3ba5986
Ansh Gupta <48497677+anshgupta2000@users.noreply.github.com>
2023-06-30 01:15:56 -0700

    adding working code for passenger_main_menu_screen.dart

--- Aireasy/lib/screens/passenger/passenger_main_menu_screen.dart
+++ Aireasy/lib/screens/passenger/passenger_main_menu_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import '../login/signup_screen.dart';
+import './user_profile_screen.dart';
+import './passenger_list_screen.dart';
+import './in_flight_services_screen.dart';
 
 class PassengerMainMenuScreen extends StatefulWidget {
   const PassengerMainMenuScreen({Key? key}) : super(key: key);
@@ -10,7 +12,7 @@ class PassengerMainMenuScreen extends StatefulWidget {
 }
 
 class _PassengerMainMenuScreenState extends State<PassengerMainMenuScreen> {
-  bool _hasPNR = false;
+  bool _hasPNR = false; // make sure to set this based on PNR check
 
   @override
   void initState() {
@@ -28,21 +30,94 @@ class _PassengerMainMenuScreenState extends State<PassengerMainMenuScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Passenger Main Menu"),
-      ),
-      body: _hasPNR
-          ? Center(
-              child: Text("Welcome! Feel free to explore."),
-            )
-          : Center(
-              child: ElevatedButton(
-                onPressed: () => Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignUpScreen()),
-                ),
-                child: Text('Enter PNR to Proceed'),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 30, // Adjusted to fit in AppBar
+              backgroundImage: AssetImage('assets/logo.jpeg'),
+            ),
+            SizedBox(width: 10),
+            Text(
+              'AirEasy',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
+          ],
+        ),
+        leading: IconButton(
+          icon: const CircleAvatar(
+            backgroundImage: AssetImage(
+                'assets/blank_profile.png'), // replace with user profile picture if exists
+          ),
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    UserProfileScreen()), // Removed 'const' here
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              SizedBox(
+                width: double.infinity,
+                height: 130,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: _hasPNR
+                      ? () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const PassengerListScreen()),
+                          )
+                      : null, // disable button if no PNR
+                  child: const Text(
+                    'Chat with Passengers',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 100),
+              SizedBox(
+                width: double.infinity,
+                height: 130,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: _hasPNR
+                      ? () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const InFlightServicesScreen()),
+                          )
+                      : null, // disable button if no PNR
+                  child: const Text(
+                    'Request Drinks, Snacks or Assistance',
+                    style: TextStyle(fontSize: 26),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
